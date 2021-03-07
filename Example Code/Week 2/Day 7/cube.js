7e6d96f338bd64b057be331e2759dabca46a9aa5
Fahmi Alfareza <fahmialfareza@icloud.com>
2021-03-07 19:16:27 +0700

    Example code for Week 2 Day 7

---
+++ Example Code/Week 2/Day 7/cube.js
@@ -0,0 +1,26 @@
+const index = require("./index"); // Import index to run rl on this file
+
+// Function to calculate cube volume
+function calculateVolumeCube(length) {
+  return length ** 3;
+}
+
+// Function to check is empty or just space
+function isEmptyOrSpaces(str) {
+  return str === null || str.match(/^ *$/) !== null;
+}
+
+// Function to input the length
+function input() {
+  index.rl.question(`Length: `, (length) => {
+    if (!isNaN(length) && !isEmptyOrSpaces(length)) {
+      console.log(`Cube's volume is ${calculateVolumeCube(length)}\n`);
+      index.rl.close();
+    } else {
+      console.log(`Length must be number`);
+      input();
+    }
+  });
+}
+
+module.exports.input = input; // Export the input, so the another file can run this code
