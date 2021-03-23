4ae68ef30553059576e850ec0eceb1cca9abc851
Fahmi Alfareza <fahmialfareza@icloud.com>
2021-03-23 12:31:45 +0700

    Get All Transaksi

--- Morning Class/Week 4/Day 18/index.js
+++ Morning Class/Week 4/Day 18/index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app = express();
 
 // Import route
-const transaksiRoute = require("./routes/transaksiRoute");
+const transaksiRoutes = require("./routes/transaksiRoutes");
 
 // Use to read the req.body
 app.use(express.urlencoded({ extended: false }));
 
 // Make route
-app.use("/transaksi", transaksiRoute);
+app.use("/transaksi", transaksiRoutes);
 
 // Server run on port 3000
 app.listen(3000, () => console.log("This server running on 3000"));
