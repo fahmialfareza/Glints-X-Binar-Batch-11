f25ac0de5f0215aeb27b1220f1dad7631d317c06
Fahmi Alfareza <fahmialfareza@icloud.com>
2021-03-25 11:48:43 +0700

    Morning Day 18

--- Morning Class/Week 4/Day 18/controllers/transaksiController.js
+++ Morning Class/Week 4/Day 18/controllers/transaksiController.js
@@ -25,6 +25,28 @@ const getAll = (req, res) => {
   });
 };
 
+const getOne = (req, res) => {
+  let sqlGetOne =
+    "SELECT t.id, p.nama as nama_pelanggan, b.nama as nama_barang, b.harga, pem.nama as nama_pemasok, t.waktu, t.jumlah, t.total FROM transaksi t JOIN barang b ON t.id_barang = b.id JOIN pelanggan p ON p.id = t.id_pelanggan JOIN pemasok pem ON b.id_pemasok = pem.id WHERE t.id = ?";
+
+  connection.query(sqlGetOne, [req.params.id], (err, results) => {
+    // If error, it will go to here
+    if (err) {
+      return res.status(500).json({
+        message: "Internal Server Error",
+        error: err,
+      });
+    }
+
+    // If no error, it will go here
+    return res.status(200).json({
+      message: "Success",
+      data: results[0],
+    });
+  });
+};
+
+// Create data
 const create = (req, res) => {
   // Find price of barang
   let sqlFindBarang = "SELECT * FROM barang WHERE id = ?";
@@ -65,6 +87,61 @@ const create = (req, res) => {
           }
 
           // If success
+          return res.status(201).json({
+            message: "Success",
+            data: results[0],
+          });
+        });
+      }
+    );
+  });
+};
+
+// Update
+const update = (req, res) => {
+  // Find barang
+  let sqlFindBarang = "SELECT * FROM barang WHERE id = ?";
+
+  // Run find query
+  connection.query(sqlFindBarang, [req.body.id_barang], (err, results) => {
+    let price = eval(results[0].harga);
+    let total = eval(req.body.jumlah * price);
+
+    let sqlUpdate =
+      "UPDATE transaksi SET id_barang = ?, id_pelanggan = ?, jumlah = ?, total = ? WHERE id = ?";
+
+    connection.query(
+      sqlUpdate,
+      [
+        req.body.id_barang,
+        req.body.id_pelanggan,
+        req.body.jumlah,
+        total,
+        req.params.id,
+      ],
+      (err, results) => {
+        // If error
+        if (err) {
+          return res.status(500).json({
+            message: "Internal Server Error",
+            error: err,
+          });
+        }
+
+        // If success
+        let sqlGetOne =
+          "SELECT t.id, p.nama as nama_pelanggan, b.nama as nama_barang, b.harga, pem.nama as nama_pemasok, t.waktu, t.jumlah, t.total FROM transaksi t JOIN barang b ON t.id_barang = b.id JOIN pelanggan p ON p.id = t.id_pelanggan JOIN pemasok pem ON b.id_pemasok = pem.id WHERE t.id = ?";
+
+        connection.query(sqlGetOne, [req.params.id], (err, results) => {
+          // If error, it will go to here
+          if (err) {
+            return res.status(500).json({
+              message: "Internal Server Error",
+              error: err,
+            });
+          }
+
+          // If no error, it will go here
           return res.status(200).json({
             message: "Success",
             data: results[0],
@@ -75,6 +152,7 @@ const create = (req, res) => {
   });
 };
 
+// Delete data
 const deleteData = (req, res) => {
   let sqlDelete = "DELETE FROM transaksi WHERE id = ?";
 
@@ -94,4 +172,4 @@ const deleteData = (req, res) => {
   });
 };
 
-module.exports = { getAll, create, deleteData }; // Export getAll function
+module.exports = { getAll, create, update, deleteData, getOne }; // Export getAll function
--- Morning Class/Week 4/Day 18/routes/transaksiRoutes.js
+++ Morning Class/Week 4/Day 18/routes/transaksiRoutes.js
@@ -7,7 +7,8 @@ const transaksiController = require("../controllers/transaksiController");
 // Define routes
 router.get("/", transaksiController.getAll);
 router.post("/", transaksiController.create);
-
+router.get("/:id", transaksiController.getOne);
+router.put("/:id", transaksiController.update);
 router.delete("/:id", transaksiController.deleteData);
 
 module.exports = router; // Export router
