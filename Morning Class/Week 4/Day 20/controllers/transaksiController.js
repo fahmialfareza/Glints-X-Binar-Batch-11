0bf7d4b027be27a24a747c623d6f92e8a2d668ec
Fahmi Alfareza <fahmialfareza@icloud.com>
2021-03-26 16:40:56 +0700

    Add getAll Transaksi

--- Morning Class/Week 4/Day 20/controllers/transaksiController.js
+++ Morning Class/Week 4/Day 20/controllers/transaksiController.js
@@ -1,17 +1,22 @@
-const { transaksi, barang, pelanggan, pemasok } = require("../models");
-require("../utils/associations");
+const { transaksi, barang, pelanggan, pemasok } = require("../models"); // Import all models
+require("../utils/associations"); // Import table relationship
 
 class TransaksiController {
+  // Get all transaksi data
   async getAll(req, res) {
     try {
       let data = await transaksi.findAll({
         // find all data of Transaksi table
         attributes: ["id", "jumlah", "total", ["createdAt", "waktu"]], // just these attributes that showed
         include: [
+          // Include is join
           {
             model: barang,
             attributes: ["nama"], // just this attrubute from Barang that showed
-            include: [{ model: pemasok, attributes: ["nama"] }],
+            include: [
+              // Include is join
+              { model: pemasok, attributes: ["nama"] },
+            ],
           },
           {
             model: pelanggan,
--- Morning Class/Week 4/Day 20/routes/transaksiRoutes.js
+++ Morning Class/Week 4/Day 20/routes/transaksiRoutes.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const router = express.Router();
+const express = require("express"); // Import express
+const router = express.Router(); // Make a router
 
 // Import controller
 const transaksiController = require("../controllers/transaksiController");
 
-router.get("/", transaksiController.getAll);
+router.get("/", transaksiController.getAll); // If GET (/transaksi), will go to getAll function in transaksiController class
 
-module.exports = router;
+module.exports = router; // Export router
