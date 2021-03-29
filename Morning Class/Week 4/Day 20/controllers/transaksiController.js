99f08437748dda3dc6802fa9e6339353d39f2d21
Fahmi Alfareza <fahmialfareza@icloud.com>
2021-03-29 12:06:06 +0700

    Create Data

--- Morning Class/Week 4/Day 20/controllers/transaksiController.js
+++ Morning Class/Week 4/Day 20/controllers/transaksiController.js
@@ -1,5 +1,4 @@
 const { transaksi, barang, pelanggan, pemasok } = require("../models"); // Import all models
-require("../utils/associations"); // Import table relationship
 
 class TransaksiController {
   // Get all transaksi data
@@ -12,7 +11,7 @@ class TransaksiController {
           // Include is join
           {
             model: barang,
-            attributes: ["nama"], // just this attrubute from Barang that showed
+            attributes: ["nama", "harga"], // just this attrubute from Barang that showed
             include: [
               // Include is join
               { model: pemasok, attributes: ["nama"] },
@@ -25,7 +24,118 @@ class TransaksiController {
         ],
       });
 
+      if (data.length === 0) {
+        return res.status(404).json({
+          message: "Data Not Found",
+        });
+      }
+
       return res.status(200).json({
+        message: "Success",
+        data,
+      });
+    } catch (e) {
+      return res.status(500).json({
+        message: "Internal Server Error",
+        error: e,
+      });
+    }
+  }
+
+  // Get One transaksi
+  getOne(req, res) {
+    // Promise
+    transaksi
+      .findOne({
+        where: { id: `${req.params.id}` },
+        attributes: ["id", "jumlah", "total", ["createdAt", "waktu"]], // just these attributes that showed
+        include: [
+          // Include is join
+          {
+            model: barang,
+            attributes: ["nama", "harga"], // just this attrubute from Barang that showed
+            include: [
+              // Include is join
+              { model: pemasok, attributes: ["nama"] },
+            ],
+          },
+          {
+            model: pelanggan,
+            attributes: ["nama"], // just this attrubute from Pelanggan that showed
+          },
+        ],
+      })
+      .then((data) => {
+        if (!data) {
+          return res.status(404).json({
+            message: "Data Not Found",
+          });
+        }
+
+        return res.status(200).json({
+          message: "Success",
+          data: data,
+        });
+      })
+      .catch((e) => {
+        return res.status(500).json({
+          message: "Internal Server Error",
+          error: e,
+        });
+      });
+  }
+
+  async create(req, res) {
+    try {
+      // Find price of barang
+      let findData = await Promise.all([
+        barang.findOne({
+          where: { id: req.body.id_barang },
+        }),
+        pelanggan.findOne({
+          where: { id: req.body.id_pelanggan },
+        }),
+      ]);
+
+      if (!findData[0] || !findData[1]) {
+        return res.status(404).json({
+          message: "Barang or Pelanggan Not Found",
+        });
+      }
+
+      // Find total
+      let price = findData[0].harga;
+      let total = eval(price * req.body.jumlah);
+
+      let createData = await transaksi.create({
+        id_barang: req.body.id_barang,
+        id_pelanggan: req.body.id_pelanggan,
+        jumlah: req.body.jumlah,
+        total,
+      });
+
+      // Find the new transaksi
+      let data = await transaksi.findOne({
+        where: { id: createData.id },
+        attributes: ["id", "jumlah", "total", ["createdAt", "waktu"]], // just these attributes that showed
+        include: [
+          // Include is join
+          {
+            model: barang,
+            attributes: ["nama", "harga"], // just this attrubute from Barang that showed
+            include: [
+              // Include is join
+              { model: pemasok, attributes: ["nama"] },
+            ],
+          },
+          {
+            model: pelanggan,
+            attributes: ["nama"], // just this attrubute from Pelanggan that showed
+          },
+        ],
+      });
+
+      return res.status(201).json({
         message: "Success",
         data,
       });
--- Morning Class/Week 4/Day 20/index.js
+++ Morning Class/Week 4/Day 20/index.js
@@ -17,6 +17,9 @@ app.use(
 // set static assets to public directory (usually for images, videos, and other files)
 app.use(express.static("public"));
 
+// Import table relationship
+require("./utils/associations");
+
 app.use("/transaksi", transaksiRoutes); // if accessing localhost:3000/transaksi/* we will go to transaksiRoutes
 
 // Server running
