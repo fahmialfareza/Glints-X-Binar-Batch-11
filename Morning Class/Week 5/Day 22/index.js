42524c0169caf0c0460fd062492f99573cbbcf8b
Fahmi Alfareza <fahmialfareza@icloud.com>
2021-03-31 12:52:36 +0700

    Morning Class Day 23

---
+++ Morning Class/Week 5/Day 22/index.js
@@ -0,0 +1,24 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+}) // Config environment
+const express = require("express"); // Import express
+
+// Import router
+const transaksiRoutes = require("./routes/transaksiRoutes");
+
+// Make express app
+const app = express();
+
+// Enable body parser
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+// Make routes
+app.use("/transaksi", transaksiRoutes);
+
+// Run server
+app.listen(3000, () => console.log("Server running on 3000!"));
