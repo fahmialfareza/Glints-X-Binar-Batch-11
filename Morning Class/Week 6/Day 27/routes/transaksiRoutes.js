71d9ba3067b671ca7a1a62f402e1df9615236834
Fahmi Alfareza <fahmialfareza@icloud.com>
2021-04-08 12:01:04 +0700

    Day 29 Morning

--- Morning Class/Week 6/Day 27/routes/transaksiRoutes.js
+++ Morning Class/Week 6/Day 27/routes/transaksiRoutes.js
@@ -13,18 +13,38 @@ const auth = require("../middlewares/auth");
 const router = express.Router();
 
 // Get all transaksi data
-router.get("/", auth.admin, transaksiController.getAll);
+router.get("/", auth.user, transaksiController.getAll);
 
 // Get one transaksi
-router.get("/:id", transaksiValidator.getOne, transaksiController.getOne);
+router.get(
+  "/:id",
+  auth.user,
+  transaksiValidator.getOne,
+  transaksiController.getOne
+);
 
 // Create transaksi
-router.post("/", transaksiValidator.create, transaksiController.create);
+router.post(
+  "/",
+  auth.admin,
+  transaksiValidator.create,
+  transaksiController.create
+);
 
 // Update transaksi
-router.put("/:id", transaksiValidator.update, transaksiController.update);
+router.put(
+  "/:id",
+  auth.admin,
+  transaksiValidator.update,
+  transaksiController.update
+);
 
 // Delete transaksi
-router.delete("/:id", transaksiValidator.delete, transaksiController.delete);
+router.delete(
+  "/:id",
+  auth.admin,
+  transaksiValidator.delete,
+  transaksiController.delete
+);
 
 module.exports = router;
