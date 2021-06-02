47d39ee9565abfd94b586735dd9d7efb3a82fedc
Fahmi Alfareza <fahmialfareza@icloud.com>
2021-06-02 15:32:57 +0700

    Fixing Auth Service

--- auth-service/controllers/authController.js
+++ auth-service/controllers/authController.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken"); // import jsonwebtoken
+const { user } = require("../models");
 
 class AuthController {
   async getToken(req, res, next) {
@@ -24,10 +25,7 @@ class AuthController {
       }
 
       // If success
-      return res.status(200).json({
-        message: "Success",
-        token,
-      });
+      return res.status(200).json({ token });
     } catch (e) {
       return next(e);
     }
@@ -38,6 +36,32 @@ class AuthController {
       return res.status(200).json({
         id: req.user.id,
       });
+    } catch (error) {
+      return next(error);
+    }
+  }
+
+  async getUserProfile(req, res, next) {
+    try {
+      const data = await user.findOne({
+        where: { id: req.params.id },
+        attributes: { exclude: ["password", "role"] },
+      });
+
+      return res.status(200).json({ data });
+    } catch (error) {
+      return next(error);
+    }
+  }
+
+  async getMyProfile(req, res, next) {
+    try {
+      const data = await user.findOne({
+        where: { id: req.user.id },
+        attributes: { exclude: ["password"] },
+      });
+
+      return res.status(200).json({ data });
     } catch (error) {
       return next(error);
     }
--- auth-service/routes/authRoute.js
+++ auth-service/routes/authRoute.js
@@ -8,6 +8,7 @@ const authController = require("../controllers/authController");
 
 // Import auth (middleware)
 const auth = require("../middlewares/auth");
+const { getMyProfile } = require("../controllers/authController");
 
 // Make router
 const router = express.Router();
@@ -36,5 +37,11 @@ router.get("/isUser", auth.user, authController.getIdUser);
 
 // Check admin or user
 router.get("/isAdminOrUser", auth.adminOrUser, authController.getIdUser);
+
+// Get My Profile
+router.get("/me", auth.adminOrUser, getMyProfile);
+
+// Get User Profile
+router.get("/profile/:id", authController.getUserProfile);
 
 module.exports = router;
